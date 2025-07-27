6856a891295dc1d4228ef9847501ca6f98bc7198
Brandon Bleau <bmbleau@gmail.com>
2025-07-27 12:26:08 -0400

    feature: add construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import spinner from "./fidget-spinner.png";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router";
+
+import logo from "./logo.png";
+import { Icon, Toolbar } from "@activityeducation/component-library";
+import "./App.css";
+
+// pages
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Router>
+        <Toolbar
+          style={{
+            backgroundColor: "rgb(243, 239, 233)",
+            height: "32px",
+            padding: "6px 16px",
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            style={{ maxHeight: "32px", maxWidth: "32px" }}
+            src={logo}
+            alt=""
+          />
+          <span style={{ fontWeight: "bold" }}>EduPub</span>
+          <nav className="toolbarNav">
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  Dashboard
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/profile"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                >
+                  Profile
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </Toolbar>
+        <div>
+          {/* A <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,12 @@
+import spinner from "../../fidget-spinner.png";
+
+function Profile() {
+  return (
+    <header className="App-header">
+      <img src={spinner} className="App-logo" alt="logo" />
+      <p>Please pardon our dust.</p>
+    </header>
+  );
+}
+
+export default Profile;
